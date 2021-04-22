b9c199860c9243c52f557acd5039743130ae0c9c
Miriam Matolcsi <miriam@matolcsi.com>
2021-04-22 21:23:11 +0200

    04.22.

--- HajosTeszt/HajosTeszt/wwwroot/Hetedik.js
+++ HajosTeszt/HajosTeszt/wwwroot/Hetedik.js
@@ -1,5 +1,6 @@
 ﻿var Kérdés;
 var sorszám = 1;
+var kérdésszám;
 
 
 function kérdésBetöltés(id) {
@@ -12,11 +13,22 @@ function kérdésBetöltés(id) {
                 return response.json()
             }
         })
-        .then(data => kérdésMegjelenítés(data));
+        .then(data => kérdésMegjelenítés(data))
+         
+}
+
+function kérdések() {
+    fetch(`/questions/all`)
+        .then(response => response.json())
+        .then(data => összeskérdés(data))
+}
+
+function összeskérdés(data) {
+    kérdésszám = Object.keys(data).length
+
 }
 
 function kérdésMegjelenítés(kérdés) {
-    console.log(kérdés);
     Kérdés = kérdés;
     document.getElementById("kérdés_szöveg").innerText = kérdés.questionText
     document.getElementById("válasz1").innerText = kérdés.answer1
@@ -31,7 +43,7 @@ function kérdésMegjelenítés(kérdés) {
 }
 
 function előre() {
-    if ((4) == sorszám) {
+    if ((kérdésszám) == sorszám) {
         sorszám = 1;
     } else {
         sorszám += 1;
@@ -44,7 +56,7 @@ function előre() {
 
 function vissza() {
     if (sorszám == 1) {
-        sorszám = 4;
+        sorszám = kérdésszám;
     } else {
         sorszám -= 1;
     }
@@ -86,6 +98,7 @@ function ellenőrzés3() {
 
 window.onload = function () {
     kérdésBetöltés(sorszám);
+    kérdések();
     document.getElementById("vissza").onclick = vissza;
     document.getElementById("előre").onclick = előre;
     document.getElementById("válasz1").onclick = ellenőrzés1;
